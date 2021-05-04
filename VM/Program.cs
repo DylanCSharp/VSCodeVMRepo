eab742857715b5f63d57afc47573690113fab078
DylanCSharp <69509219+DylanCSharp@users.noreply.github.com>
2021-05-04 10:08:45 +0200

    Add files via upload

---
+++ VM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Dylans VM!");
+        }
+    }
+}
